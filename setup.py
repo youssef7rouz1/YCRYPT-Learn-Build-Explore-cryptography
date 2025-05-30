b385bf7fea323a81908c537cfd522b014828e8f5
YoussefBA <92054880+youssef7rouz1@users.noreply.github.com>
2025-05-30 16:57:01 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="YCrypt",
+    version="0.1.0",
+    packages=find_packages(),  # will pick up algorithms, utils, tests, etc.
+)
